0ead716e2fa63f4151addbcb9c3f127f15aebfb6
Ian Bovard <thebestpantsintown@gmail.com>
2017-08-12 19:59:00 -1000

    prop header

--- src/components/BookListAppTitle.js
+++ src/components/BookListAppTitle.js
@@ -1,14 +1,5 @@
 import React, { Component } from 'react';
 
-class BookListAppTitle extends Component{
-/*  constructor(props) {
-    super(props);
-  }*/
-  render() {
-    return(
-      <h3>Reactive Book List</h3>
-      );
-  }
-}
+const BookListAppTitle = ({ appTitle }) => <h3>{ appTitle }</h3>
 
 export default BookListAppTitle;
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 import BookListAppTitle from '../../components/BookListAppTitle';
-import { getBooksFromFakeXHR } from '../../lib/books.db.js';
+/*import { getBooksFromFakeXHR } from '../../lib/books.db.js';*/
 import './styles.css';
 
 class AppContainer extends Component{
-/*  constructor(){
-    super();
-  }*/
+ constructor(props){
+    super(props);
+    this.appTitle = 'Reactive Book List';
+  }
+
+
   render(){
     return (
       <div className="AppContainer">
-      <BookListAppTitle />
+      <BookListAppTitle appTitle ={ this.appTitle }/>
       </div>
       );
   }
